38104b3a6ad93cad76eef5ad4afa074818b3f87f
Raymond Yee <raymond.yee@gmail.com>
2016-04-06 12:46:18 -0700

    add methods that return books from latest release into GitenbergJob

--- gitenberg_utils.py
+++ gitenberg_utils.py
@@ -1,13 +1,22 @@
-__all__ = ['GitenbergJob']
+__all__ = ['GitenbergJob', 'GitenbergTravisJob', 'ForkBuildRepo', 'BuildRepo', 'BuildRepo2']
 
 # import github3.py
+
+import base64
+import datetime
+import yaml
+
 import arrow
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
 import github3
+from github3.repos.release import Release
+
 from gitenberg import metadata
 import requests
 import semantic_version
-import yaml
-
+from travispy import TravisPy
 
 from second_folio import (travis_template, slugify)
 
@@ -202,4 +211,254 @@ class GitenbergJob(object):
             
         return (commit, path_content is None)
 
+    def release_from_tag(self, tag_name):
+        """Get a release by tag name.
+        release_from_tag() returns a release with specified tag
+        while release() returns a release with specified release id
+        :param str tag_name: (required) name of tag
+        :returns: :class:`Release <github3.repos.release.Release>`
+        """
+        
+        repo = self.gh_repo
+        url = repo._build_url('releases', 'tags', tag_name,
+                              base_url=repo._api)
+        json = repo._json(repo._get(url), 200)
+        return Release(json, repo) if json else None
 
+
+    def ebooks_in_github_release(self, tag):
+        """
+        returns a list of (book_type, book_name) for a given GitHub release (specified by 
+        owner, name, tag).  token is a GitHub authorization token -- useful for accessing
+        higher rate limit in the GitHub API
+        """
+
+        # epub, mobi, pdf, html, text
+        # map mimetype to file extension
+        EBOOK_FORMATS = {'application/epub+zip':'epub',
+                     'application/x-mobipocket-ebook': 'mobi',
+                     'application/pdf': 'pdf',
+                     'text/plain': 'text',
+                     'text/html':'html'}
+
+
+        release = self.release_from_tag(tag)
+
+        return [(EBOOK_FORMATS.get(asset.content_type), asset.name) 
+                for asset in release.iter_assets() 
+                if EBOOK_FORMATS.get(asset.content_type) is not None]   
+
+class GitenbergTravisJob(GitenbergJob):
+    def __init__(self, username, password, repo_name, repo_owner,
+               update_travis_commit_msg,
+               tag_commit_message, travis_token=None, repo_token=None):
+        
+        super(GitenbergTravisJob, self).__init__(username, password, repo_name, repo_owner,
+               update_travis_commit_msg,
+               tag_commit_message)
+        
+        self.username = username
+        self.password = password
+        
+        self._travis_token = travis_token
+        if travis_token is None:
+            self._travis_token = self.travis_token()
+            
+        self._repo_token = repo_token    
+        self._travis_repo_public_key = None
+ 
+        self.travis = TravisPy.github_auth(self.travis_token())
+        if self.gh_repo is not None:
+            self.travis_repo = self.travis.repo(self.repo_slug)
+       
+        
+    def public_key_for_travis_repo(self):
+        if self._travis_repo_public_key is None:
+            self._travis_repo_public_key =  requests.get("https://api.travis-ci.org/repos/{}/{}/key".format(self.repo_owner,
+                                        self.repo_name)).json()['key']
+        return self._travis_repo_public_key
+
+
+    def travis_token(self):
+
+        if self._travis_token is not None:
+            return self._travis_token
+        
+        token_note = "token for travis {}".format(datetime.datetime.utcnow().isoformat())
+        token = self.gh.authorize(self.username, self.password, 
+                             scopes=('read:org', 'user:email', 'repo_deployment', 
+                                     'repo:status', 'write:repo_hook'), note=token_note)
+
+        return token.token
+    
+    def repo_token(self, from_repo_owner='GITenberg', create_duplicate=False):
+        """
+       
+        """
+
+        if self._repo_token is not None:
+            return self._repo_token
+        
+        token_note = "automatic releases for {}/{}".format(self.repo_owner, self.repo_name)
+
+        try:
+            token = self.gh.authorize(username, password, scopes=('public_repo'), note=token_note)
+        except Exception as e:
+            if self._authorization_description_already_exists(e):
+                # try again with a new description
+                if create_duplicate:
+                    token_note += " " + datetime.datetime.utcnow().isoformat()
+                    token = self.gh.authorize(username, password, scopes=('public_repo'), note=token_note)
+                else:
+                    raise Exception('repo token already exists for {}'.format(self.repo_slug))
+            else:
+                raise e
+
+        self._repo_token = token.token
+        return self._repo_token
+    
+    def travis_encrypt(self, token_to_encrypt):
+        """
+        return encrypted version of token_to_encrypt 
+        """
+        
+        # token_to_encrypt has to be string
+        # if's not, assume it's unicode and enconde in utf-8
+        
+        if isinstance(token_to_encrypt, unicode):
+            token_string = token_to_encrypt.encode('utf-8')
+        else:
+            token_string = token_to_encrypt
+        
+        repo_public_key_text = self.public_key_for_travis_repo() 
+        repo_public_key = serialization.load_pem_public_key(repo_public_key_text.encode('utf-8'),
+                                                            backend=default_backend())
+
+        ciphertext = repo_public_key.encrypt(
+         token_string,
+         padding.PKCS1v15()
+        )
+
+        return base64.b64encode(ciphertext)
+    
+    
+    @staticmethod
+    def _authorization_description_already_exists(e):
+        """
+        Given an exception e when trying to create a token, is the exception the result of a duplicate description
+        """
+        if (e.code == 422 and 
+            e.message == u'Validation Failed' and 
+            (u'already_exists', u'description') in [(error['code'], error['field']) for error in e.errors]):
+            return True
+        else:
+            return False
+
+class ForkBuildRepo(GitenbergTravisJob):
+    def fork_and_build_gitenberg_repo(self, from_repo_owner='GITenberg', 
+                                      create_duplicate_token=False,
+                                      update_repo_token_file=True):
+        """
+
+        """
+
+        from_repo = self.gh.repository(from_repo_owner, self.repo_name)
+        
+        # fork if necessary
+        if self.gh_repo is None:
+            self.gh_repo = from_repo.create_fork()
+            # instantiate self.travis_repo
+            self.travis.user().sync()
+            
+            self.travis_repo = self.travis.repo(self.repo_slug)
+           
+        
+        # make sure it's active
+        if not self.travis_repo.enable():
+            raise Exception("unable to enable travis repo:{}".format(self.repo_slug))
+    
+        encrypted_key = self.travis_encrypt(self.repo_token())
+        
+        # update .travis.deploy.api_key.txt if requested
+        if update_repo_token_file:
+            self.create_or_update_file(
+                 path = ".travis.deploy.api_key.txt", 
+                 message = "update .travis.deploy.api_key.txt with new encrypted token",
+                 content = encrypted_key,
+                 ci_skip = True)
+            
+        #  update .travis.yml
+        
+        self.create_or_update_file(
+            path = ".travis.yml",
+            message = "update .travis.yml with new token and repo_owner",
+            content = self.update_travis_template(write_changes=False, 
+                                encrypted_key=encrypted_key)[0].encode('utf-8'),
+            ci_skip = True
+        )
+
+        # update version and tag commit -- should fire off a travis build
+        tag_result = self.tag_commit(version_type='patch', write_changes=True)
+        return tag_result
+        
+class BuildRepo(GitenbergTravisJob):
+        
+    def run(self, from_repo_owner='GITenberg', 
+                                      create_duplicate_token=False,
+                                      update_repo_token_file=True,
+                                      load_repo_token=True):
+        """
+
+        """
+
+        
+        # make sure it's active
+        if not self.travis_repo.enable():
+            raise Exception("unable to enable travis repo:{}".format(self.repo_slug))
+    
+        if load_repo_token:
+            try:
+                encrypted_key = self.gh_repo.contents(".travis.deploy.api_key.txt").decoded
+            except:
+                encrypted_key = self.travis_encrypt(self.repo_token())
+        else:
+            encrypted_key = self.travis_encrypt(self.repo_token())
+        
+        # update .travis.deploy.api_key.txt if requested
+        if update_repo_token_file:
+            self.create_or_update_file(
+                 path = ".travis.deploy.api_key.txt", 
+                 message = "update .travis.deploy.api_key.txt with new encrypted token",
+                 content = encrypted_key,
+                 ci_skip = True)
+            
+        #  update .travis.yml
+        
+        self.create_or_update_file(
+            path = ".travis.yml",
+            message = "update .travis.yml with new token and repo_owner",
+            content = self.update_travis_template(write_changes=False, 
+                                encrypted_key=encrypted_key)[0].encode('utf-8'),
+            ci_skip = True
+        )
+
+        # update version and tag commit -- should fire off a travis build
+        tag_result = self.tag_commit(version_type='patch', write_changes=True)
+        return tag_result
+    
+class BuildRepo2(BuildRepo):
+    def next_version(self, version_type='patch'):
+        """
+        can be overriden -- by default next patch
+        """
+
+        assert version_type in ('patch', 'minor', 'major')
+
+        import semantic_version
+        
+        _version = self.version()
+        if _version < semantic_version.Version('0.1.0'):
+            return semantic_version.Version('0.1.0')
+        else:
+            next_func = getattr(_version, "next_{}".format(version_type))
+            return next_func()
