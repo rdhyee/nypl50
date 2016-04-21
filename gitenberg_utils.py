9cff9929d392e17a78ebc3a14daccce020518038
Raymond Yee <raymond.yee@gmail.com>
2016-04-20 17:22:07 -0700

    move MetadataWrite to gitenberg_utils.py

--- gitenberg_utils.py
+++ gitenberg_utils.py
@@ -13,6 +13,8 @@ from cryptography.hazmat.backends import default_backend
 import github3
 from github3.repos.release import Release
 
+import gitenberg
+
 from gitenberg import metadata
 import requests
 import semantic_version
@@ -35,7 +37,79 @@ class GitenbergJob(object):
         self.tag_commit_message = tag_commit_message
 
         self._metadata = None
+        self._root_tree = None
 
+    def asciidoc_in_repo_root(self, branch ='master'):
+        """return list of asciidocs in the root of repo"""
+ 
+        return [hash_.path 
+                for hash_ in self.root_tree(branch)
+                if hash_.path.endswith('.asciidoc')]
+
+    def create_or_update_file(self, path, message, content, branch='master', ci_skip=True):
+    
+        message += ' [ci skip]' if ci_skip else ''
+        
+        path_content =  self.gh_repo.contents(path, ref=branch)
+        
+        if path_content is None:
+            data = {
+                'path': path,
+                'message': message,
+                'content': content,
+                'branch': branch
+            }
+
+            commit = self.gh_repo.create_file(**data)
+  
+        else:
+            data = {
+                'message': message,
+                'content': content,
+            }
+
+            commit = path_content.update(**data)
+            
+        return (commit, path_content is None)
+
+    def ebooks_in_github_release(self, tag):
+        """
+        returns a list of (book_type, book_name) for a given GitHub release (specified by 
+        owner, name, tag).  token is a GitHub authorization token -- useful for accessing
+        higher rate limit in the GitHub API
+        """
+
+        # epub, mobi, pdf, html, text
+        # map mimetype to file extension
+        EBOOK_FORMATS = {'application/epub+zip':'epub',
+                     'application/x-mobipocket-ebook': 'mobi',
+                     'application/pdf': 'pdf',
+                     'text/plain': 'text',
+                     'text/html':'html'}
+
+
+        release = self.release_from_tag(tag)
+        if release is not None:
+            assets = release.iter_assets()
+            return [(EBOOK_FORMATS.get(asset.content_type), asset.name) 
+                    for asset in assets 
+                    if EBOOK_FORMATS.get(asset.content_type) is not None]   
+        else:
+            return []
+
+    def root_tree (self, branch='master'):
+        
+        if self._root_tree is None:
+            repo = self.gh_repo
+            repo_branch = repo.branch(branch)
+            tree = repo.tree(repo_branch.commit.sha)
+            self._root_tree = tree.tree
+
+        return self._root_tree
+    
+    def in_root_tree(self, path):
+        return path in [hash_.path for hash_ in self.root_tree()]
+        
     def update_travis_and_commit (self, write_changes=True, update_travis=True, tag_commit=True):
         if update_travis:
             (template_result, template_written, commit) =  self.update_travis_template(write_changes=write_changes)
@@ -62,12 +136,13 @@ class GitenbergJob(object):
             # self._metadata = metadata.pandata.Pandata(metadata_url)
 
             self._metadata = metadata.pandata.Pandata(datafile=None)
-            self._metadata.metadata = yaml.safe_load(self.gh_repo.contents("metadata.yaml").decoded)
-
+            md_hash = self.gh_repo.contents("metadata.yaml")
+            # if the file exists, load the content
+            if md_hash is not None:
+                self._metadata.metadata = yaml.safe_load(md_hash.decoded)
             return self._metadata
         else:
             return self._metadata
-
         
     def update_travis_template(self, write_changes=True, template=None, encrypted_key=None):
         """
@@ -112,8 +187,11 @@ class GitenbergJob(object):
         return (template_result, template_written, commit)
 
     def version(self):
-        md = self.metadata()
-        return semantic_version.Version(md.metadata.get("_version"))
+        try:
+            md = self.metadata()
+            return semantic_version.Version(md.metadata.get("_version"))
+        except:
+            return None
 
     def next_version(self, version_type='patch'):
         """
@@ -122,9 +200,12 @@ class GitenbergJob(object):
 
         assert version_type in ('patch', 'minor', 'major')
 
-        _version = self.version()
-        next_func = getattr(_version, "next_{}".format(version_type))
-        return next_func()
+        try:
+            _version = self.version()
+            next_func = getattr(_version, "next_{}".format(version_type))
+            return next_func()
+        except:
+            return None
         
     def tag_commit(self, version_type='patch', write_changes=True):
         """
@@ -171,46 +252,6 @@ class GitenbergJob(object):
 
         return (_version, _next_version, metadata_updated, commit, tag)
     
-    def url_latest_epub(self):
-        """
-        repo is a github3.py repo
-        """
-        
-        md = self.metadata()
-        epub_title = slugify(md.metadata.get("title"))
-        tag = md.metadata.get("_version")
-        url = "https://github.com/GITenberg/{}/releases/download/{}/{}.epub".format(self.repo_name, tag, epub_title)
-        return url
-    
-    def status_latest_epub(self):
-        return requests.head(self.url_latest_epub()).status_code
-
-    def create_or_update_file(self, path, message, content, branch='master', ci_skip=True):
-    
-        message += ' [ci skip]' if ci_skip else ''
-        
-        path_content =  self.gh_repo.contents(path, ref=branch)
-        
-        if path_content is None:
-            data = {
-                'path': path,
-                'message': message,
-                'content': content,
-                'branch': branch
-            }
-
-            commit = self.gh_repo.create_file(**data)
-  
-        else:
-            data = {
-                'message': message,
-                'content': content,
-            }
-
-            commit = path_content.update(**data)
-            
-        return (commit, path_content is None)
-
     def release_from_tag(self, tag_name):
         """Get a release by tag name.
         release_from_tag() returns a release with specified tag
@@ -225,29 +266,42 @@ class GitenbergJob(object):
         json = repo._json(repo._get(url), 200)
         return Release(json, repo) if json else None
 
+    def status (self):
+        
+        files_of_interest = ['metadata.yaml', 'book.asciidoc', 'cover.jpg', 
+                            '.travis.yml', '.travis.deploy.api_key.txt']
+        
+        version = self.version()
+        ebooks_in_latest_github_release = self.ebooks_in_github_release(tag=unicode(version)) \
+              if version is not None else []
+        
+        _status = {
+            'repo_owner': self.repo_owner,
+            'repo_name': self.repo_name,
+            'asciidocs': self.asciidoc_in_repo_root(),
+            'version': unicode(version),
+            'ebooks_in_latest_github_release': ebooks_in_latest_github_release,
+            'ebooks_in_release_count': len(ebooks_in_latest_github_release)
+        }
+        
+        _status.update(dict([(path, self.in_root_tree(path)) for path in files_of_interest]))
 
-    def ebooks_in_github_release(self, tag):
+        return _status
+    
+    def status_latest_epub(self):
+        return requests.head(self.url_latest_epub()).status_code
+
+    def url_latest_epub(self):
         """
-        returns a list of (book_type, book_name) for a given GitHub release (specified by 
-        owner, name, tag).  token is a GitHub authorization token -- useful for accessing
-        higher rate limit in the GitHub API
+        repo is a github3.py repo
         """
-
-        # epub, mobi, pdf, html, text
-        # map mimetype to file extension
-        EBOOK_FORMATS = {'application/epub+zip':'epub',
-                     'application/x-mobipocket-ebook': 'mobi',
-                     'application/pdf': 'pdf',
-                     'text/plain': 'text',
-                     'text/html':'html'}
-
-
-        release = self.release_from_tag(tag)
-
-        return [(EBOOK_FORMATS.get(asset.content_type), asset.name) 
-                for asset in release.iter_assets() 
-                if EBOOK_FORMATS.get(asset.content_type) is not None]   
-
+        
+        md = self.metadata()
+        epub_title = slugify(md.metadata.get("title"))
+        tag = md.metadata.get("_version")
+        url = "https://github.com/GITenberg/{}/releases/download/{}/{}.epub".format(self.repo_name, tag, epub_title)
+        return url
+ 
 class GitenbergTravisJob(GitenbergJob):
     def __init__(self, username, password, repo_name, repo_owner,
                update_travis_commit_msg,
@@ -302,13 +356,14 @@ class GitenbergTravisJob(GitenbergJob):
         token_note = "automatic releases for {}/{}".format(self.repo_owner, self.repo_name)
 
         try:
-            token = self.gh.authorize(username, password, scopes=('public_repo'), note=token_note)
+            token = self.gh.authorize(self.username, self.password, scopes=('public_repo'), note=token_note)
         except Exception as e:
+            raise e # for now
             if self._authorization_description_already_exists(e):
                 # try again with a new description
                 if create_duplicate:
                     token_note += " " + datetime.datetime.utcnow().isoformat()
-                    token = self.gh.authorize(username, password, scopes=('public_repo'), note=token_note)
+                    token = self.gh.authorize(self.username, self.password, scopes=('public_repo'), note=token_note)
                 else:
                     raise Exception('repo token already exists for {}'.format(self.repo_slug))
             else:
@@ -462,3 +517,45 @@ class BuildRepo2(BuildRepo):
         else:
             next_func = getattr(_version, "next_{}".format(version_type))
             return next_func()
+
+
+"""
+https://github.com/rdhyee/gitberg
+had to download https://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2
+and configure /Users/raymondyee/Library/Application Support/gitberg/config.yaml (Mac)
+to pick up Gutenberg metadata 
+"""
+
+def repo_md(repo_id):
+
+    b = gitenberg.Book(repo_id)
+    b.parse_book_metadata()
+
+    return b.meta.metadata 
+
+class MetadataWrite(GitenbergJob):
+    
+    def run(self):
+        try:
+            import gitenberg
+
+            repo_id = self.repo_name.split("_")[-1]
+            md = repo_md(repo_id)
+            # add version
+            md["_version"] = "0.0.1"
+
+            # add keywords 
+            subjects = md.get("subjects")
+            if 'GITenberg' not in subjects:
+                subjects.append('GITenberg')
+            md["subjects"] = subjects
+
+            result = self.create_or_update_file(
+                             path = "metadata.yaml", 
+                             message = "write metadata.yaml",
+                             content = yaml.safe_dump(md,default_flow_style=False,
+                                  allow_unicode=True),
+                             ci_skip = True)
+            return (self.repo_name, result)
+        except Exception as e:
+            return (self.repo_name, e)
